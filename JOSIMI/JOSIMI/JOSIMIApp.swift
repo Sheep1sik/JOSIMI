b33cffae670a16d8f10209402526965c023b009b
_GGDol <bohg6799@naver.com>
2024-11-18 00:28:49 +0900

    Init: Set up project with essential configurations
    - 프로젝트 초기 셋업 완료
    - 필수 디렉토리 및 파일 구조 구성

---
+++ JOSIMI/JOSIMI/JOSIMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JOSIMIApp.swift
+//  JOSIMI
+//
+//  Created by 양원식 on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct JOSIMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LogInView()
+        }
+    }
+}
